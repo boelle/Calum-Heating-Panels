bc28c860f371d3f579a040b89497196be362c17c
Bo Herrmannsen <bo.herrmannsen@gmail.com>
2020-08-14 22:28:11 +0200

    Update speed-emoncms.py

--- speed-emoncms.py
+++ speed-emoncms.py
@@ -3,14 +3,11 @@
 
 #SETTINGS START
 
-# Domain you want to post to: localhost would be an emoncms installation on your own laptop
-# this could be changed to emoncms.org to post to emoncms.org
+# Domain you want to post to, this could be changed to emoncms.net to post to emoncms.net
 domain = "takeaguess"
-domain1 = "takeaguess"
-
 
 # Location of emoncms in your server, the standard setup is to place it in a folder called emoncms
-# To post to emoncms.org change this to blank: ""
+# To post to emoncms.net change this to blank: ""
 emoncmspath = "emoncms"
 
 # Write apikey of emoncms account
@@ -51,34 +48,14 @@ ping[0] = ping[0].replace(',', '.')
 download[0] = download[0].replace(',', '.')
 upload[0] = upload[0].replace(',', '.')
 
-
-
 def emoncms():
 
     try:
         seq = (ping[0], download[0], upload[0])
         str_join = ",".join(str(x) for x in seq)
         print time.asctime( time.localtime(time.time()) ),
-        print ' Preparing Data for local server'
+        print ' Preparing data for hosted server'
         conn = httplib.HTTPConnection(domain)
-
-        conarg1 = ('/', emoncmspath, '/input/post?node=', str(nodeid), '&csv=', str_join, '&apikey=', apikey)
-        conarg = "".join(str(x) for x in conarg1)
-
-        conn.request("GET", conarg)
-
-        response = conn.getresponse()
-        print time.asctime( time.localtime(time.time()) ),
-        print ' Response from local server:',
-        print response.read()
-        print time.asctime( time.localtime(time.time()) ),
-        print ' Data sent to local server'
-        
-        seq = (ping[0], download[0], upload[0])
-        str_join = ",".join(str(x) for x in seq)
-        print time.asctime( time.localtime(time.time()) ),
-        print ' Preparing Data for hosted server'
-        conn = httplib.HTTPConnection(domain1)
 
         conarg1 = ('/', emoncmspath, '/input/post?node=', str(nodeid), '&csv=', str_join, '&apikey=', apikey)
         conarg = "".join(str(x) for x in conarg1)
@@ -91,11 +68,10 @@ def emoncms():
         print response.read()
         print time.asctime( time.localtime(time.time()) ),
         print ' Data sent to hosted server'
-        
+
     except Exception, ex:
         print time.asctime( time.localtime(time.time()) ),
         print ' This error occurred: ' + str(ex)
-
 
 def speed():
 
@@ -107,8 +83,8 @@ def speed():
         print time.asctime( time.localtime(time.time()) ),
         print ' This error occurred: ' + str(ex)
 
-    #print '{},{},{},{},{}'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping[0], download[0], upload[0])
-    #print ping, download, upload
+    print '{},{},{},{},{}'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping[0], download[0], upload[0])
+    print ping, download, upload
 
 
 speed()
